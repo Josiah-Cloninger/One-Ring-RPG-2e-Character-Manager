73d5cee28fe3d85e68cb1dc449e77a6186be24ac
josiah.cloninger@gmail.com <josiah.cloninger@gmail.com>
2024-12-10 13:37:37 -0600

    started on main run loop

--- test.py
+++ test.py
@@ -4,14 +4,3 @@ from culture2 import Cultures2
 from gear2 import Weapons2, Armours2, Headgears2, Shields2
 
 
-input_str = "strength: 5, heart: 7, wits: 2"
-
-attributes_list = input_str.split(", ")
-
-attributes_dict = {}
-
-for attribute in attributes_list:
-    attribute_name, attribute_value = attribute.split(": ")
-    attributes_dict[attribute_name] = int(attribute_value)
-
-print(attributes_dict)
