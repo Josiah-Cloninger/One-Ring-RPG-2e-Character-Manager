26175e32fc22cd8120a8c7c5cf73505298d092b8
josiah.cloninger@gmail.com <josiah.cloninger@gmail.com>
2024-12-10 18:36:31 -0600

    fixed scholar calling

--- calling2.py
+++ calling2.py
@@ -67,7 +67,9 @@ class Callings2(metaclass=MetaEnum):
                      "and wonder, songs composed in ages past strengthen the weariest of hearts. A love of learning guides your every step, and illuminates the "
                      "way for you and those who listen to your advice."),
         favoured_skills=["craft", "lore", "riddle"],
-        distinctive_feature="Rhymes of Lore"
+        distinctive_feature="Rhymes of Lore",
+        feature_description=("Rhymes of Lore are short verses created by many Cultures to help them remember significant facts from ancient history which might "
+                             "otherwise be lost. Scholars of Middle-earth credit much of their ancient knowledge to such rhymes."),
         shadow_path="Lure of Secrets",
         shadow_path_description=("Inquisitiveness and curiosity are desirable virtues in an individual, but knowledge can be put to malicious use, and learned "
                                  "individuals can look down on others as ignorant fools. Secrets are danger- ous, as the very desire of uncovering them may corrupt the heart.")
