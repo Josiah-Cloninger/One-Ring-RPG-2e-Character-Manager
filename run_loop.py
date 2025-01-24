07d0fac79919141741e8c7fd2decc7055772a2d5
josiah.cloninger@gmail.com <josiah.cloninger@gmail.com>
2025-01-24 11:12:42 -0600

    Updated show_attribute to be able to change attributes of objects of complex classes. Changed version to 1.0

--- run_loop.py
+++ run_loop.py
@@ -1,6 +1,8 @@
 from ui_functions import (clear_console, start_help, create_character, 
                           select_character_to_load, save_current_character, 
-                          show_attribute, roll_skill)
+                          show_attribute, set_attribute, roll_skill)
+
+from character import (load_character)
 
 
 active_character = None
@@ -10,10 +12,10 @@ clear_console()
 while True:
     if active_character is None:
         print("Please start by either loading an exhisting character with 'load' or creating a new character with 'create'")
-        user_command = input("> ").lower()
-        user_command = user_command.split()
-        user_command.extend([None]*(10 - len(user_command)))
-        match user_command[0]:
+        user_commands = input("> ").lower()
+        user_commands = user_commands.split()
+        user_commands.extend([None]*(10 - len(user_commands)))
+        match user_commands[0]:
             case "help":
                 start_help()
             case "exit":
@@ -21,15 +23,15 @@ while True:
             case "create":
                 active_character = create_character()
             case "load":
-                active_character = select_character_to_load(user_command[1])
+                active_character = select_character_to_load(user_commands[1])
             case _:
                 print("Invalid command\n")
     else:
         print("\nEnter a command:")
-        user_command = input("> ").lower()
-        user_command = user_command.split()
-        user_command.extend([None]*(10 - len(user_command)))
-        match user_command[0]:
+        user_commands = input("> ").lower()
+        user_commands = user_commands.split()
+        user_commands.extend([None]*(10 - len(user_commands)))
+        match user_commands[0]:
             case "help":
                 help()
             case "exit":
@@ -37,12 +39,14 @@ while True:
             case "create":
                 active_character = create_character()
             case "load":
-                active_character = select_character_to_load(user_command[1])
+                active_character = select_character_to_load(user_commands[1])
             case "save":
                 save_current_character(active_character)
             case "show":
-                show_attribute(active_character, user_command[1])
+                show_attribute(active_character, user_commands)
+            case "set":
+                set_attribute(active_character, user_commands)
             case "roll":
-                roll_skill(active_character, user_command[1])
+                roll_skill(active_character, user_commands[1])
             case _:
                 print("Invalid command\n")
